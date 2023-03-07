d74b4740fb2ed2b2a96b1f77d60617af53356823
AlbertQWJ <3214563036@qq.com>
2023-03-07 16:08:02 +0000

    Add Icon and Application Launch Animations

---
+++ app/src/main/java/com/example/myapplicationexample/SplashScreen.java
@@ -0,0 +1,68 @@
+package com.example.myapplicationexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000; //Time Controller
+
+    //Hooks
+    View first,second,third,fourth,fifth,sixth,seventh,logo;
+    TextView slogan;
+
+    //Animations
+    Animation topAnimation,bottomAnimation,middleAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this,R.anim.middle_animation);
+
+        //Hooks
+        first = findViewById(R.id.first_line);
+        second = findViewById(R.id.second_line);
+        third = findViewById(R.id.third_line);
+        fourth = findViewById(R.id.fourth_line);
+        fifth = findViewById(R.id.fifth_line);
+        sixth = findViewById(R.id.sixth_line);
+        seventh = findViewById(R.id.seventh_line);
+
+        logo = findViewById(R.id.logo);
+        slogan = findViewById(R.id.tagLine);
+
+        first.setAnimation(topAnimation);
+        second.setAnimation(topAnimation);
+        third.setAnimation(topAnimation);
+        fourth.setAnimation(topAnimation);
+        fifth.setAnimation(topAnimation);
+        sixth.setAnimation(topAnimation);
+        seventh.setAnimation(topAnimation);
+
+        logo.setAnimation(middleAnimation);
+        slogan.setAnimation(bottomAnimation);
+
+        //Splash Screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
